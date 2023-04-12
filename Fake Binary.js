ffde045d3ab8b02dbec450d84c1e02f56b1f8987
Rajesh Arora <rajesharora2801@gmail.com>
2023-04-12 20:41:19 +0530

    CodeOfTheDay-12Apr2023

---
+++ Fake Binary.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+//  -----MySolution-----
+function fakeBin(x){
+    let newStr = x.split('').map(item => item<5 ? 0:1).join('')
+     return newStr
+   }
