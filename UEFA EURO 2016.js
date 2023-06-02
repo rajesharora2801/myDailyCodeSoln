b4fe66573542fec6aa64f5c1560ecb7ea16fd627
Rajesh Arora <rajesharora2801@gmail.com>
2023-06-02 18:36:13 +0530

    CodeOfTheDay-02-June-2023

---
+++ UEFA EURO 2016.js
@@ -0,0 +1,23 @@
+// DESCRIPTION:
+// Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+// uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+// uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+// uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+//  -----MySolution-----
+function uefaEuro2016(teams, scores){
+    let result = "";
+    if (scores[0] > scores[1]) {
+      result = `${teams[0]} won!`
+    } else if (scores[0] < scores[1]) {
+      result = `${teams[1]} won!`
+    } else {
+      result = "teams played draw."
+    }
+    return `At match ${teams[0]} - ${teams[1]}, ${result}`
+  }
+
+//  -----Other Good Solution-----
+const uefaEuro2016 = (teams, scores) =>
+  `At match ${teams[0]} - ${teams[1]}, ${scores[0] === scores[1] ? `teams played draw.` : `${scores[0] > scores[1] ? teams[0] : teams[1]} won!`}`;
